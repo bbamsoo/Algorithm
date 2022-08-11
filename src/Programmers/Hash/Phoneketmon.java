3d5ba75a206e049efbc3bfc6d9e12ad2d9069024
bbamsooexit <hiaro66@naver.com>
2022-08-11 15:21:04 +0900

    폰켓몬

---
+++ src/Programmers/Hash/Phoneketmon.java
@@ -0,0 +1,19 @@
+package Programmers.Hash;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Phoneketmon {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int select = nums.length/2;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int n:nums)
+            map.put(n,map.getOrDefault(n, 0)+1);
+        if(map.size()<select)
+            answer = map.size();
+        else
+            answer = select;
+
+        return answer;
+    }
+}
