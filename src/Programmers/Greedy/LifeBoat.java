76850c7058e86336b1dacfced9f036a4b9b56973
bbamsooexit <hiaro66@naver.com>
2022-08-12 11:02:04 +0900

    구명보트

---
+++ src/Programmers/Greedy/LifeBoat.java
@@ -0,0 +1,18 @@
+package Programmers.Greedy;
+
+import java.util.Arrays;
+
+public class LifeBoat {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int min=0;
+        Arrays.sort(people);
+
+        for(int max=people.length-1;max>=min;max--) {
+            if(people[min]+people[max]<=limit)
+                min++;
+            answer++;
+        }
+        return answer;
+    }
+}
