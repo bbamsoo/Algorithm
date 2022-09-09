92ef5d000cdb781ad54bb34a851af8410c065e52
bbamsooexit <hiaro66@naver.com>
2022-09-09 11:40:18 +0900

    N과 M(4)

---
+++ src/BAEKJOON/BackTracking/BAEKJOON15652.java
@@ -0,0 +1,37 @@
+package BAEKJOON.BackTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON15652 {
+    public static int N,M;
+    public static int[]arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr=new int[M];
+        dfs(1,0);
+        System.out.println(sb);
+    }
+    public static void dfs(int num, int depth){
+        if (depth==M){
+            for(int n:arr){
+                sb.append(n).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i=num;i<=N;i++){
+            arr[depth]=i;
+            dfs(i,depth+1);
+        }
+    }
+}
