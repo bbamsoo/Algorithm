95db2791388cb2d36917eb878b3a615024550917
bbamsooexit <hiaro66@naver.com>
2022-09-14 14:40:32 +0900

    설탕 배달

---
+++ src/BAEKJOON/Greedy/BAEKJOON2839.java
@@ -0,0 +1,29 @@
+package BAEKJOON.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result=0;
+        while (true){
+            if(n%5==0){
+                result+=n/5;
+                break;
+            }
+            else {
+                n-=3;
+                result++;
+            }
+            if(n<0){
+                result=-1;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
