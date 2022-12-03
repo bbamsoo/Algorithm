0ca1904cbe7b640bb2a768a01dffc07ca11338e4
bbamsooexit <hiaro66@naver.com>
2022-12-03 17:40:59 +0900

    N과 M(1)

--- src/BAEKJOON/BackTracking/BAEKJOON15649.java
+++ src/BAEKJOON/BackTracking/BAEKJOON15649.java
@@ -1,16 +1,21 @@
 package BAEKJOON.BackTracking;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BAEKJOON15649 {
     static int N, M;
     static int[] arr;
     static boolean[] isUsed;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         isUsed = new boolean[N + 1];
         arr = new int[M + 1];
