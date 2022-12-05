017d52cd5df91fb45a4acce0fa7a69cf20eb156f
bbamsooexit <hiaro66@naver.com>
2022-12-05 09:36:41 +0900

    체스판 다시 칠하기

--- src/BAEKJOON/BruteForce/BAEKJOON1018.java
+++ src/BAEKJOON/BruteForce/BAEKJOON1018.java
@@ -1,24 +1,27 @@
-package BAEKJOON;
+package BAEKJOON.BruteForce;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BAEKJOON1018 {
 
     public static boolean[][] arr;
     public static int min = 64;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         arr = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            String str = sc.next();
-
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
                 if (str.charAt(j) == 'W') {
                     arr[i][j] = true;
