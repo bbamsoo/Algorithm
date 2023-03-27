a54fe26fbda928471ae9fd434090b8bba501e2ac
bbamsoo <hiaro66@naver.com>
2023-03-27 09:33:44 +0900

    ATM

--- src/BAEKJOON/Greedy/BAEKJOON11399.java
+++ src/BAEKJOON/Greedy/BAEKJOON11399.java
@@ -1,23 +1,31 @@
 package BAEKJOON.Greedy;
 
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BAEKJOON11399 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N=sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         int[]wait=new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i=0;i<N;i++)
-            wait[i]=sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            wait[i] = Integer.parseInt(st.nextToken());
+        }
         Arrays.sort(wait);
-        for(int i=1;i<N;i++)
-            wait[i]+=wait[i-1];
+        for (int i = 1; i < N; i++) {
+            wait[i] += wait[i - 1];
+        }
 
         int sum=0;
         for (int num:wait)
-            sum+=num;
-        System.out.println(sum);
+            sum += num;
+        bw.write(sum+"");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
