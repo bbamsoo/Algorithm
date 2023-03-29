e4d0f5bbfe5e1c76cfdd8202f0870296845bc6ff
bbamsoo <hiaro66@naver.com>
2023-03-29 09:36:18 +0900

    동전 0

--- src/BAEKJOON/Greedy/BAEKJOON11047.java
+++ src/BAEKJOON/Greedy/BAEKJOON11047.java
@@ -1,23 +1,30 @@
 package BAEKJOON.Greedy;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class BAEKJOON11047 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num=sc.nextInt();
-        int money=sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int num = Integer.parseInt(st.nextToken());
+        int money = Integer.parseInt(st.nextToken());
         int count=0;
 
         int []changes=new int[num];
-        for(int i=0;i<num;i++)
-            changes[i]=sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            changes[i] = Integer.parseInt(br.readLine());
+        }
 		for(int i=changes.length-1;i>=0;i--) {
             if(money>=changes[i]) {
                 count += (money / changes[i]);
                 money %= changes[i];
             }
 		}
-		System.out.println(count);
+		bw.write(count+"");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
